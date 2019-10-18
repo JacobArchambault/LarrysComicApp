75626867ddb7ab3b2def2996cccaff79229e2ae2
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-18 08:58:21 -0400

    Eliminate check for if i == 0 in firstWay() and the redundant call inside it. Add check for if i == 0 as a disjunction to check for if i is a first element.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ namespace LarrysComicApp
             firstWay(testList);
         }
 
-        // FIXME: ArgumentOutOfRangeException prevents me from printing a dash when I have a string of consecutive numbers right at the start.
         static void firstWay(List<int> l)
         {
             // 1. Write opening parenthesis
@@ -21,31 +20,17 @@ namespace LarrysComicApp
             // 2. Cycle through the elements in list, comparing each element in place i to it's immediately preceding and following elements to check for consecutiveness.
             for (int i = 0; i < l.Count; i++)
             {
+                // 2.1 Print the last number regardless (including it in the logic immediately below throws an Argument out of range exception.)
                 if (i == l.Count - 1)
-                {
                     Console.Write($"{l[i]}");
-                }
-                else if (i == 0)
-                {
-                    if (l[i + 1] - l[i] > 1)
-                    {
-                        Console.Write($"{l[i]}, ");
-                    }
-                    else
-                    {
-                        Console.Write($"{l[i]}-");
-                    }
-                }
-                // 2.1.1 If i is a last element in a sequence of consecutive numbers, print it followed by a comma.
+
+                // 2.2 If i is a last element in a sequence of consecutive numbers (that's not last in the list), print it followed by a comma.
                 else if (l[i + 1] - l[i] > 1)
-                {
                     Console.Write($"{l[i]}, ");
-                }
-                // 2.1.2 if i is a first element (that's not also a last element, since that's covered by the previous case), print it followed by a dash.
-                else if (l[i] - l[i - 1] > 1)
-                {
+
+                // 2.3 if i is a first element (that's not also a last element, since that's covered by the previous case), print it followed by a dash.
+                else if (i == 0 || l[i] - l[i - 1] > 1)
                     Console.Write($"{l[i]}-");
-                }
             }
             // 3. Write closing parenthesis.
             Console.Write("}");
@@ -53,15 +38,14 @@ namespace LarrysComicApp
 
         static void secondWay(List<int> l)
         {
-
+            // Way 2: 
+            // From set of numbers, obtain a set of sets of consecutive numbers.
+            // print {
+            // for each set s in setOfsets
+            // if it has only one member, print that member,
+            // if it has more than one member, print firstMember-lastMember
+            // if member isn't the last, print a comma
+            // print }
         }
     }
 }
-// Way 2: 
-// From set of numbers, obtain a set of sets of consecutive numbers.
-// print {
-// for each set s in setOfsets
-// if it has only one member, print that member,
-// if it has more than one member, print firstMember-lastMember
-// if member isn't the last, print a comma
-// print }
