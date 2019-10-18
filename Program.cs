d7224417f794d320ce2ae493228c429ad3212527
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-18 08:14:57 -0400

    Fixed all bugs, but includes inelegant try/catch.

--- Program.cs
+++ Program.cs
@@ -9,18 +9,10 @@ namespace LarrysComicApp
         {
             Console.WriteLine("***** Larry's amazing comic book filer app! *****");
             // 1. Generate mock list to work with.
-            List<int> testList = new List<int> { 1, 2, 4, 5, 6, 9, 10, 11, 12, 15, 49, 50, 100, 111, 112 };
+            List<int> testList = new List<int> { 1, 2, 4, 5, 6, 9, 10, 11, 12, 15, 49, 50, 100, 110, 112 };
             firstWay(testList);
-
-            // 4. Check if the values are equal, and increment lastValue if so
-            // 4.1 If lastValue - firstValue >= 2, print firstValue + ", "
-            // 4.2 Set firstValue equal to last value
-            // 1.all elements are consecutive, and;
-            // if 
-
-            // 4.1 if they are, set 
-            // 4.2 if they aren't.
-
+            List<int> testList2 = new List<int> { 1 };
+            //firstWay(testList2);
         }
 
         // FIXME: ArgumentOutOfRangeException prevents me from printing a dash when I have a string of consecutive numbers right at the start.
@@ -33,12 +25,12 @@ namespace LarrysComicApp
             {
                 try
                 {
-                    // 2.1.1 If i is a last element, print it followed by a comma.
+                    // 2.1.1 If i is a last element in a sequence of consecutive numbers, print it followed by a comma.
                     if (l[i + 1] - l[i] > 1)
                     {
                         Console.Write($"{l[i]}, ");
                     }
-                    // 2.1.2 if i is a first element (that's not a last element, since that's covered by the previous case), print it followed by a dash.
+                    // 2.1.2 if i is a first element (that's not also a last element, since that's covered by the previous case), print it followed by a dash.
                     else if (l[i] - l[i - 1] > 1)
                     {
                         Console.Write($"{l[i]}-");
@@ -49,8 +41,13 @@ namespace LarrysComicApp
                     // 2.1 if i is the first or last element in list l, print it regardless.
                     Console.Write($"{l[i]}");
                     // If it's not the last in the list, print a comma after it.
-                    if (i != l.Count - 1)
-                        Console.Write(", ");
+                    if (i != l.Count -1)
+                    {
+                        if (l[i + 1] - l[i] > 1)
+                            Console.Write(", ");
+                        else
+                            Console.Write("-");
+                    }
                 }
             }
             // 3. Write closing parenthesis.
