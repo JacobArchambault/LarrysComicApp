b3a57cae44de03a00e56dbf25d9846debd57030e
Jacob Archambault <jacob.archambault@waystar.com>
2019-10-17 21:31:01 -0400

    Working project for every case EXCEPT where first element in list is first of several consecutive elements.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace LarrysComicApp
         {
             Console.WriteLine("***** Larry's amazing comic book filer app! *****");
             // 1. Generate mock list to work with.
-            List<int> testList = new List<int> { 1, 4, 5, 6, 9, 10, 11, 12, 49, 50, 100, 111, 112 };
+            List<int> testList = new List<int> { 1, 2, 4, 5, 6, 9, 10, 11, 12, 15, 49, 50, 100, 111, 112 };
             firstWay(testList);
 
             // 4. Check if the values are equal, and increment lastValue if so
@@ -22,88 +22,52 @@ namespace LarrysComicApp
             // 4.2 if they aren't.
 
         }
+
+        // FIXME: ArgumentOutOfRangeException prevents me from printing a dash when I have a string of consecutive numbers right at the start.
         static void firstWay(List<int> l)
         {
             // 1. Write opening parenthesis
             Console.Write("{");
-            // 2. Cycle through the elements in list.
+            // 2. Cycle through the elements in list, comparing each element in place i to it's immediately preceding and following elements to check for consecutiveness.
             for (int i = 0; i < l.Count; i++)
             {
-                // If it's both a first and a last element, print it followed by a comma
                 try
                 {
-                    if (l[i] - l[i - 1] > 1 && l[i+1] - l[i] > 1)
+                    // 2.1.1 If i is a last element, print it followed by a comma.
+                    if (l[i + 1] - l[i] > 1)
                     {
                         Console.Write($"{l[i]}, ");
                     }
+                    // 2.1.2 if i is a first element (that's not a last element, since that's covered by the previous case), print it followed by a dash.
                     else if (l[i] - l[i - 1] > 1)
                     {
                         Console.Write($"{l[i]}-");
                     }
-                    else if (l[i + 1] - l[i] > 1)
-                    {
-                        Console.Write($"{l[i]}, ");
-                    }
                 }
                 catch (ArgumentOutOfRangeException)
-                {
+                {                
+                    // 2.1 if i is the first or last element in list l, print it regardless.
                     Console.Write($"{l[i]}");
+                    // If it's not the last in the list, print a comma after it.
                     if (i != l.Count - 1)
                         Console.Write(", ");
                 }
-                // If it's a first element, print it followed by -.
-                // If it's a last element, print it.
-                // If it's not the last in the list, print a comma.
-                //if (i != l.Count - 1)
-                //{
-                //    Console.Write($",");
-                //}
-                // 4.1 Check if the first number and the current number are consecutive
-                // 4.2 if so, print the first number
-                // Check if difference betwe
-
-
             }
+            // 3. Write closing parenthesis.
             Console.Write("}");
-            // 5. Write closing parenthesis.
         }
-        //bool isFirstInConsecutive(int myInt)
-        //{
-
-        //    return 
-        //}
 
         static void secondWay(List<int> l)
         {
 
         }
-        static void thirdWay(List<int> l)
-        {
-
-        }
     }
 }
-// Three ways to do this.
-// Way 1: 
-// Create a variable generator that
-// 1.1. stores all the maximal subsets in variables,
-// 1.2. Gets the leftover numbers, then 
-// Print the numbers in order
-// 1.3 if listOfInts[0] > leftOver, print $"{listOfInts[0]}-{listOfInts[last]}, {leftOver}"
-// 1.4 else print $"{leftOver}, {listOfInts[0]
-// 1.5. Move to 
-
 // Way 2: 
-// If they're equal, move the last element.
-// If they're consecutive, print the first element
-
-// Way 3: 
 // From set of numbers, obtain a set of sets of consecutive numbers.
 // print {
 // for each set s in setOfsets
 // if it has only one member, print that member,
 // if it has more than one member, print firstMember-lastMember
 // if member isn't the last, print a comma
-// else print }
-
-// four cases: first and last -> print followed by comma, first only print followed by -, last only -> print followed by comma, neither.
+// print }
